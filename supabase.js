64359fd49e1da0c94d6765e0d6b1345308a57550
julindoti <julinmestredoti@gmail.com>
2025-09-28 17:20:09 -0300

    feat(database):update for a new database

--- supabase.js
+++ supabase.js
@@ -1,7 +1,81 @@
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+
+const supabaseUrl = 'https://thfagkwulnssnpqayzhi.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoZmFna3d1bG5zc25wcWF5emhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwODMyMzgsImV4cCI6MjA3NDY1OTIzOH0.93dh-_qWKSmbnJ3OccataitHJax4nDYn5s_nn-oxI5I';
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+
+document.addEventListener("DOMContentLoaded",() =>{
+
+      document.querySelector('button[onclick="login()"]').addEventListener('click', login);
+      document.querySelector('button[onclick="cadastrar()"]').addEventListener('click', cadastrar);
+
+})
+
+ async function login() {
+        if (typeof supabase === 'undefined') {
+          document.getElementById("mensagemCadastro").innerText = "Supabase não carregado. Tente novamente.";
+          return;
+        }
+
+        console.log("Tentando fazer login...");
+        const email = document.getElementById("email").value.trim();
+        const senha = document.getElementById("senha").value.trim();
+
+        if (!email || !senha) {
+          document.getElementById("mensagemCadastro").innerText =
+            "Preencha todos os campos!";
+          return;
+        }
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+          email,
+          password: senha,
+        });
+
+        if (error) {
+          document.getElementById("mensagemCadastro").innerText =
+            "Erro: " + error.message;
+        } else {
+          document.getElementById("mensagemCadastro").innerText =
+            "Login realizado com sucesso!";
+          localStorage.setItem("usuarioEmail", email);
+
+          setTimeout(() => {
+            window.location.href = "bem-vindo.html";
+          }, 2000);
+        }
+      }
+
+      async function cadastrar() {
+        if (typeof supabase === 'undefined') {
+          document.getElementById("mensagemCadastro").innerText = "Supabase não carregado. Tente novamente.";
+          return;
+        }
+
+        console.log("Tentando cadastrar...");
+        const email = document.getElementById("email").value.trim();
+        const senha = document.getElementById("senha").value.trim();
+
+        if (!email || !senha) {
+          document.getElementById("mensagemCadastro").innerText =
+            "Preencha todos os campos!";
+          return;
+        }
+
+        const { data, error } = await supabase.auth.signUp({
+          email,
+          password: senha,
+        });
+
+        if (error) {
+          document.getElementById("mensagemCadastro").innerText =
+            "Erro: " + error.message;
+        } else {
+          document.getElementById("mensagemCadastro").innerText =
+            "Cadastro realizado com sucesso!";
+        }
+      }
 
 // const supabase = window.supabase.createClient(                                                                                                                                                                                       
 //   "https://peihaqxmkhxwwbiasgjo.supabase.co",                                                                                                                                                                                        
